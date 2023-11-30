4bf273835f06f32d5d972bf2c496d9ed03af9a54
Namal Dayarathna <namal@namal.dev>
2023-11-30 21:18:23 +0000

    Add docs

--- easom.py
+++ easom.py
@@ -1,5 +1,12 @@
 import numpy as np
 
 
-def easom(x, y):
+def easom(x: float, y: float) -> float:
+    """
+    Calculate the value of Easom function at point (x, y)
+
+    :param x: x coordinate
+    :param y: y coordinate
+    :return: value of Easom function at point (x, y)
+    """
     return -(np.cos(x) * np.cos(y) * np.exp(-(x - np.pi) ** 2 - (y - np.pi) ** 2))
