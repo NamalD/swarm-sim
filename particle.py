c14e043e3241f1949b26e913663780e755b51f7d
Namal Dayarathna <namal@namal.dev>
2023-11-30 22:01:12 +0000

    Update method WIP

--- particle.py
+++ particle.py
@@ -6,3 +6,11 @@ class Particle:
         self.position = Vector.random(min_bound, max_bound)
         self.velocity = Vector.random(min_bound, max_bound)
 
+    # TODO: Unit test this method.
+    def update(self):
+        # TODO: Update the particle's velocity.
+        self.position += self.velocity
+        # TODO: Update the particle's best-known position and fitness value.
+        # TODO: Update the global best-known position and fitness value.
+        pass
+
